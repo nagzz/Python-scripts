96e8a6159cb21377d1d4279a651d859127de6a43
bioinagesh <nagzz@users.noreply.github.com>
2022-01-25 10:36:19 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+"""
+Created on 12/01/2022
+@author: Nagesh
+"""
+
+import sys
+import os
+
+def main():
+    args = sys.argv[1:]
+    if len(args)>1 or len(args)<1:
+        print("Error: Takes only one argument")
+    else:
+        input1 = args[0]
+        print(input1)
+        print("processing")
+        try:
+            d = {}
+            with open(input1, 'r', encoding = "utf-8") as f:
+                for line in f.readlines():
+#                    print(line)
+#                    l=[line]
+#                    print(l)
+                    s=[]
+                    for i in line.split(" "):
+                        s.append(i) #convert strings to list
+#                    print(s)
+                    d=dict(x.strip().split(":") for x in s) #dictionary comprehension: converting list to dictionary
+#                    print(d)
+                    e={a: int(x) for a, x in d.items()} #dictionary comprehension: converting the values from string format to integer format
+                    print(e)
+#                    print(line.strip().split(":"))
+        except:
+            print("Process failed")
+#    print(d)
+if __name__ == "__main__":
+    sys.exit(main())
